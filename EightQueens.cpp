f59e1ab591b97247ac9095113fd01611feef811f
amirbaghi <bamirmasoud@gmail.com>
2021-01-29 15:40:51 +0330

    change background color, add lighting and zoom in/out

--- EightQueens.cpp
+++ EightQueens.cpp
@@ -7,6 +7,11 @@
 #define FLOOR_HEIGHT 9
 
 // Global Variables
+int init_zoom_time = 0.0;
+bool isZoomingIn = false;
+bool isZoomingOut = false;
+float camera_fov = 20.0;
+
 float camera_theta = 0.0;
 int init_time = 0;
 
@@ -39,12 +44,12 @@ void EightQueens::render_board()
 
     // Draw the white squares
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, board_index_vbo);
-    glColor3f(1.0, 1.0, 1.0);
+    glColor4f(0.85, 0.85, 0.85, 1.0);
     glDrawElements(GL_QUADS, sizeof(white_square_indices) / 4, GL_UNSIGNED_INT, NULL);
 
     // Draw the black squares
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, board_index_vbo + 1);
-    glColor3f(0.0, 0.0, 0.0);
+    glColor4f(0.1, 0.1, 0.1, 1.0);
     glDrawElements(GL_QUADS, sizeof(black_square_indices) / 4, GL_UNSIGNED_INT, NULL);
 
     // Draw the board outline
@@ -62,15 +67,34 @@ void EightQueens::render()
 
     camera_theta = diff_time * 2.0;
 
+    // Zoom in/out if the user is currently pressing the up or down arrow
+    if (isZoomingIn)
+    {
+        auto diff_zoom_time = (current_time - init_zoom_time) / 1000.0;
+        if (camera_fov >= 0)
+            camera_fov -= diff_time * 0.002;
+    }
+    else if (isZoomingOut)
+    {
+        auto diff_zoom_time = (current_time - init_zoom_time) / 1000.0;
+        if (camera_fov <= 100)
+            camera_fov += diff_time * 0.002;
+    }
+
     auto w = glutGet(GLUT_WINDOW_WIDTH);
     auto h = glutGet(GLUT_WINDOW_HEIGHT);
 
-    camera_config(w, h, camera_theta);
+    camera_config(w, h, camera_theta, camera_fov);
 
     glClear(GL_COLOR_BUFFER_BIT);
     glClear(GL_DEPTH_BUFFER_BIT);
 
+    glDisable(GL_LIGHTING);
+
+    // Render the board
     render_board();
+
+    glEnable(GL_LIGHTING);
 
     glutSwapBuffers();
 }
@@ -147,9 +171,26 @@ void EightQueens::init_board()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, board_index_vbo + 2);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(board_outline_indices), board_outline_indices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ARRAY_BUFFER, NULL);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, NULL);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
 
+void EightQueens::init_light()
+{
+    glEnable(GL_LIGHTING);
+
+    glEnable(GL_LIGHT0);
+
+    // Candle Yellow Light
+    float light0_diffuse[] = {255.0 / 255.0, 147.0 / 255.0, 41.0 / 255.0, 1.0};
+    float light0_specular[] = {255.0 / 255.0, 147.0 / 255.0, 41.0 / 255.0, 1.0};
+    float light0_ambient[] = {255.0 / 255.0, 147.0 / 255.0, 41.0 / 255.0, 1.0};
+    float light0_pos[] = {3, 3, 3, 1};
+
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light0_specular);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light0_ambient);
+    glLightfv(GL_LIGHT0, GL_POSITION, light0_pos);
 }
 
 void EightQueens::init()
@@ -158,22 +199,27 @@ void EightQueens::init()
 
     init_time = glutGet(GLUT_ELAPSED_TIME);
 
-    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glClearColor(0.2, 0.2, 0.2, 0.0);
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     // Initializing the board
     init_board();
 
     glEnableClientState(GL_VERTEX_ARRAY);
+
+    // Initializing the lightning
+    init_light();
 }
 
-void EightQueens::camera_config(int w, int h, float t)
+void EightQueens::camera_config(int w, int h, float t, float fov)
 {
     glViewport(0, 0, w, h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 
-    gluPerspective(20.0, (float)w / (float)h, 2.0, 500.0);
+    gluPerspective(fov, (float)w / (float)h, 2.0, 500.0);
 
     gluLookAt(
         10, 10, 20,
@@ -185,13 +231,53 @@ void EightQueens::camera_config(int w, int h, float t)
 
 void EightQueens::reshape(int w, int h)
 {
-    camera_config(w, h, camera_theta);
+    camera_config(w, h, camera_theta, camera_fov);
+}
+
+void EightQueens::keyboard(int key, int x, int y)
+{
+    switch (key)
+    {
+    case GLUT_KEY_UP:
+        // Start Zooming in
+        isZoomingIn = true;
+        init_zoom_time = glutGet(GLUT_ELAPSED_TIME);
+        break;
+
+    case GLUT_KEY_DOWN:
+        // Start Zooming out
+        isZoomingOut = true;
+        init_zoom_time = glutGet(GLUT_ELAPSED_TIME);
+        break;
+
+    default:
+        break;
+    }
+}
+
+void EightQueens::keyboard_up(int key, int x, int y)
+{
+    switch (key)
+    {
+    case GLUT_KEY_UP:
+        // Stop Zooming in
+        isZoomingIn = false;
+        break;
+
+    case GLUT_KEY_DOWN:
+        // Stop Zooming out
+        isZoomingOut = false;
+        break;
+
+    default:
+        break;
+    }
 }
 
 int EightQueens::main(int argc, char **argv)
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowSize(800, 600);
     glutCreateWindow("Eight Queens");
 
@@ -200,5 +286,7 @@ int EightQueens::main(int argc, char **argv)
     glutDisplayFunc(render);
     glutIdleFunc(render);
     glutReshapeFunc(reshape);
+    glutSpecialFunc(keyboard);
+    glutSpecialUpFunc(keyboard_up);
     glutMainLoop();
 }
--- Headers/EightQueens.h
+++ Headers/EightQueens.h
@@ -1,6 +1,12 @@
 namespace EightQueens
 {
 
+    // Keyboard Function
+    void keyboard(int key, int x, int y);
+
+    // Keyboard Up Function
+    void keyboard_up(int key, int x, int y);
+
     // Render Function
     void render();
 
@@ -12,12 +18,15 @@ namespace EightQueens
 
     // Function for initializing the chess board arrays and data
     void init_board();
+
+    // Funciton for initializing the lightning
+    void init_light();
     
     // Main Function
     int main(int argc, char **argv);
 
     // Camera Configuration Function
-    void camera_config(int w, int h, float t);
+    void camera_config(int w, int h, float t, float fov);
 
     // Reshape Function
     void reshape(int w, int h);
