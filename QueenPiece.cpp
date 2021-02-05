a16783cd7085e9ad67132cfe18f59a03078653d6
amirbaghi <bamirmasoud@gmail.com>
2021-02-05 18:47:43 +0330

    change obj file directory

--- QueenPiece.cpp
+++ QueenPiece.cpp
@@ -7,7 +7,7 @@ QueenPiece::QueenPiece(){
 
 QueenPiece::QueenPiece(int row, int col, int name, vertex pos) : QueenPiece()
 {
-    this->model = Object3D("./EightQueens/Resources/queen.obj");
+    this->model = Object3D("./Resources/queen.obj");
     this->model.setPosition(pos);
     this->model.setColor(color4(0.0, 0.0, 0.0, 1.0));
     this->model.name = name;
